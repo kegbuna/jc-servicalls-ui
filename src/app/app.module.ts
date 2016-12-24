5f765ed77fd2a0984a19b1b54ef959704893392d
Ken Egbuna <kegbuna@gmail.com>
2016-12-24 11:55:28 -0500

    starting up project, including angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+import { CallsService } from './calls.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
-  providers: [],
+  providers: [CallsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
