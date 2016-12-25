05aee9c5dae5bf33f21ffa06462f027b5f625827
Ken Egbuna <kegbuna@gmail.com>
2016-12-25 01:53:43 -0500

    Adding routes and angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material'
+import 'hammerjs';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material'
+import {RouterModule, Routes} from '@angular/router';
 
-import { CallsService } from './calls.service';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+
+import {CallsService} from './calls.service';
+import { MapComponent } from './map/map.component';
+import { HomeComponent } from './home/home.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MapComponent,
+    HomeComponent,
+    SideNavComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [CallsService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
